0332913eee994db1243c04a7867de8add26cc322
Alex Gates-Shannon <alex.nebula99@gmail.com>
2020-04-16 19:23:51 -0400

    Load and animate external models

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,27 @@
 import * as Three from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+import { GLTF, GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
 
 const ROOT_ID = 'cartographer-v1-root';
+const SCALE_FACTOR = 0.1;
+
+const mixers: Three.AnimationMixer[] = [];
+const clock = new Three.Clock();
 
 let camera: Three.PerspectiveCamera | null = null;
 let controls: OrbitControls | null = null;
-let mesh: Three.Mesh | null = null;
 let renderer: Three.WebGLRenderer | null = null;
 let root: HTMLDivElement | null = null;
 let scene: Three.Scene | null = null;
 
 function createCamera(): void {
   if (!root) { throw new Error() }
-  const fov = 35;
+  const fov = 50;
   const aspect = root.clientWidth / root.clientHeight;
-  const near = 0.1;
+  const near = 10;
   const far = 100;
   camera = new Three.PerspectiveCamera(fov, aspect, near, far);
-  camera.position.set(-4, 4, 10);
+  camera.position.set(-15, 15, 50);
 }
 
 function createControls(): void {
@@ -31,14 +35,6 @@ function createLights(): void {
   const mainLight = new Three.DirectionalLight( 0xffffff, 5.0 );
   mainLight.position.set( 10, 10, 10 );
   scene.add( ambientLight, mainLight );
-}
-
-function createMeshes(): void {
-  if (!scene) { throw new Error(); }
-  const geometry = new Three.BoxBufferGeometry(2, 2, 2);
-  const material = new Three.MeshStandardMaterial({ color: 0x800080 });
-  mesh = new Three.Mesh(geometry, material);
-  scene.add(mesh);
 }
 
 function createRenderer(): void {
@@ -71,7 +67,7 @@ function init(): void {
   createCamera();
   createControls();
   createLights();
-  createMeshes();
+  loadModels();
   createRenderer();
 
   if (!renderer) { throw new Error(); }
@@ -81,13 +77,49 @@ function init(): void {
   });
 }
 
+function loadModels(): void {
+  const loader = new GLTFLoader();
+
+  const handleLoad = (gltf: GLTF, position: Three.Vector3): void => {
+    if (!scene) { throw new Error(); }
+
+    const model = gltf.scene.children[ 0 ];
+    model.position.copy( position );
+
+    const scaleM = model.matrix.clone().makeScale(SCALE_FACTOR, SCALE_FACTOR, SCALE_FACTOR);
+    model.applyMatrix4(scaleM);
+
+    const animation = gltf.animations[ 0 ];
+
+    const mixer = new Three.AnimationMixer( model );
+    mixers.push( mixer );
+
+    const action = mixer.clipAction( animation );
+    action.play();
+
+    scene.add( model );
+  };
+
+  const FUDGE = 3;
+  const parrotPosition = new Three.Vector3(0, 0, 2.5 * FUDGE / SCALE_FACTOR);
+  loader.load('models/parrot.glb', (gltf: GLTF) => handleLoad(gltf, parrotPosition));
+  const flamingoPosition = new Three.Vector3( 7.5 * FUDGE / SCALE_FACTOR, 0, -10 * FUDGE / SCALE_FACTOR );
+  loader.load('models/flamingo.glb', (gltf: GLTF) => handleLoad(gltf, flamingoPosition));
+  const storkPosition = new Three.Vector3( 0, -2.5 * FUDGE / SCALE_FACTOR, -10 * FUDGE / SCALE_FACTOR );
+  loader.load('models/stork.glb', (gltf: GLTF) => handleLoad(gltf, storkPosition));
+}
+
 function render(): void {
   if (!renderer || !scene || !camera) { throw new Error(); }
   renderer.render( scene, camera );
 }
 
 function update(): void {
-  // Currently empty
+  // if (!mixers || mixers.length === 0) { throw new Error(); }
+  const delta = clock.getDelta();
+  for (const mixer of mixers) {
+    mixer.update(delta);
+  }
 }
 
 window.addEventListener( 'resize', handleWindowResize );
