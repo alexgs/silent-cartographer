5e02832b6b91b82bbe3ea9eb99683d46e9cef854
Alex Gates-Shannon <alex.nebula99@gmail.com>
2020-04-15 23:21:09 -0400

    Add first example from `react-three-fiber` README

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,22 @@
-import React from 'react';
+import * as React from 'react';
 import { hot } from 'react-hot-loader/root';
+import { Canvas } from 'react-three-fiber';
 
 import './App.css';
+import { Box } from './Box';
 
 // eslint-disable-next-line @typescript-eslint/no-empty-interface
 interface Props {}
 
 const App: React.FunctionComponent<Props> = () => {
-  return <div>Hello Cartographer!</div>;
+  return (
+    <Canvas>
+      <ambientLight />
+      <pointLight position={[10, 10, 10]} />
+      <Box position={[-1.2, 0, 0]} />
+      <Box position={[1.2, 0, 0]} />
+    </Canvas>
+  );
 };
 App.displayName = 'App';
 
