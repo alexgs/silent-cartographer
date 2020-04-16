00c8c49ed6356246a1e0b9689583308d097c3b5b
Alex Gates-Shannon <alex.nebula99@gmail.com>
2020-04-16 13:25:46 -0400

    Add light and action

--- src/index.ts
+++ src/index.ts
@@ -1,31 +1,61 @@
 import * as Three from 'three';
 
-const root = document.querySelector('#cartographer-v1-root');
+const ROOT_ID = 'cartographer-v1-root';
+
+let camera: Three.PerspectiveCamera;
+let mesh: Three.Mesh;
+let renderer: Three.WebGLRenderer;
+let root: HTMLDivElement | null;
+let scene: Three.Scene;
+
 const clientHeight = 500;
-if (root) {
+
+function animate(): void {
+  requestAnimationFrame( animate );
+
+  // Increase the mesh's rotation each frame
+  mesh.rotation.z += 0.01;
+  mesh.rotation.x += 0.01;
+  mesh.rotation.y += 0.01;
+
+  renderer.render( scene, camera );
+}
+
+function init(): void {
+  root = document.querySelector('#' + ROOT_ID);
+  if (!root) {
+    throw new Error(`Could not find HTML element with ID ${ROOT_ID}`);
+  }
+
   // Scene
-  const scene = new Three.Scene();
-  scene.background = new Three.Color( 'skyblue' );
+  scene = new Three.Scene();
+  scene.background = new Three.Color('skyblue');
 
   // Camera
   const fov = 35;
   const aspect = root.clientWidth / clientHeight;
   const near = 0.1;
   const far = 100;
-  const camera = new Three.PerspectiveCamera( fov, aspect, near, far );
+  camera = new Three.PerspectiveCamera(fov, aspect, near, far);
   camera.position.set(0, 0, 10);
 
-  // Box
-  const geometry = new Three.BoxBufferGeometry( 2, 2, 2 );
-  const material = new Three.MeshBasicMaterial();
-  const mesh = new Three.Mesh( geometry, material );
-  scene.add( mesh );
+  // Box geometry
+  const geometry = new Three.BoxBufferGeometry(2, 2, 2);
+  const material = new Three.MeshStandardMaterial({ color: 0x800080 });
+  mesh = new Three.Mesh(geometry, material);
+  scene.add(mesh);
+
+  // Light
+  const light = new Three.DirectionalLight( 0xffffff, 5.0 );
+  light.position.set( 10, 10, 10 );
+  scene.add( light );
 
   // Render
-  const renderer = new Three.WebGLRenderer();
-  renderer.setSize( root.clientWidth, clientHeight );
-  renderer.setPixelRatio( window.devicePixelRatio );
-  root.appendChild( renderer.domElement );
-
-  renderer.render( scene, camera );
+  renderer = new Three.WebGLRenderer({ antialias: true });
+  renderer.setSize(root.clientWidth, clientHeight);
+  renderer.setPixelRatio(window.devicePixelRatio);
+  root.appendChild(renderer.domElement);
 }
+
+init();
+animate();
