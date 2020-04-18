d75636d686bc97efe286bcb6108d512fd0eefb4b
Alex Gates-Shannon <alex.nebula99@gmail.com>
2020-04-18 11:14:41 -0400

    Load parrot model

--- src/App.tsx
+++ src/App.tsx
@@ -4,17 +4,20 @@ import { Canvas } from 'react-three-fiber';
 
 import './App.css';
 import { Box } from './Box';
+import { Model } from './Model';
 
 // eslint-disable-next-line @typescript-eslint/no-empty-interface
 interface Props {}
 
 const App: React.FunctionComponent<Props> = () => {
   return (
-    <Canvas>
+    <Canvas camera={{ fov: 60 }}>
       <ambientLight />
       <pointLight position={[10, 10, 10]} />
-      <Box position={[-1.2, 0, 0]} />
       <Box position={[1.2, 0, 0]} />
+      <React.Suspense fallback={<Box position={[0, 0, 0]} />}>
+        <Model position={[0, 0, 0]} url={'models/parrot.glb'} />
+      </React.Suspense>
     </Canvas>
   );
 };
