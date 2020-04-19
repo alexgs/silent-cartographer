5fca2a08337af99bcee1d070f3419ecdd0ce31eb
Alex Gates-Shannon <alex.nebula99@gmail.com>
2020-04-19 10:28:57 -0400

    Add basic "hello sphere" from article on npm support

--- babylon.js/src/index.ts
+++ babylon.js/src/index.ts
@@ -1,3 +1,56 @@
+import {
+  ArcRotateCamera,
+  Engine,
+  HemisphericLight,
+  Mesh,
+  MeshBuilder,
+  Scene,
+  Vector3,
+} from 'babylonjs';
+
 const CANVAS_ID = 'cartographer-v1-canvas';
 
-console.log('Hello world!');
+function getCanvas(): HTMLCanvasElement {
+  const output: HTMLElement | null = document.getElementById(CANVAS_ID);
+  if (!output) {
+    throw new Error();
+  }
+  return output as HTMLCanvasElement;
+}
+
+const canvas: HTMLCanvasElement = getCanvas();
+const engine: Engine = new Engine(canvas, true);
+
+function createScene(): Scene {
+  const scene: Scene = new Scene(engine);
+
+  const camera: ArcRotateCamera = new ArcRotateCamera(
+    'Camera',
+    Math.PI / 2,
+    Math.PI / 2,
+    2,
+    Vector3.Zero(),
+    scene,
+  );
+  camera.attachControl(canvas, true);
+
+  const light1: HemisphericLight = new HemisphericLight(
+    'light1',
+    new Vector3(1, 1, 0),
+    scene,
+  );
+
+  const sphere: Mesh = MeshBuilder.CreateSphere(
+    'sphere',
+    { diameter: 1 },
+    scene,
+  );
+
+  return scene;
+}
+
+const scene: Scene = createScene();
+
+engine.runRenderLoop(() => {
+  scene.render();
+});
